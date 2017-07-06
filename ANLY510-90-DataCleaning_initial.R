04397b694613caedfc7a9c4cf6ad1f0e111a30ea
LYDIA <lydiawuwisdom@gmail.com>
2017-07-06 16:17:38 -0500

    Update ANLY510-90-DataCleaning_initial.R

--- ANLY510-90-DataCleaning_initial.R
+++ ANLY510-90-DataCleaning_initial.R
@@ -9,4 +9,26 @@ clean$Area <- na.locf(clean$Area)
 clean$Street <- str_replace_all(clean$Street,"????"," ")
 clean$Street.2 <- str_trim(clean$Street.2)
 
+# Assignment-7-collaboration-on-repository-and-data-cleanup
+# Remove special characters, padding from Street 1 and Street 2 variables,etc.
+DataClean<-read.csv("~/Downloads/DataClean.csv")
+clean<-read.csv("~/Downloads/DataClean.csv")
+View(clean)
+clean$Street <- gsub("[^A-Za-z ]", " ", clean$Street)
+clean$Street <- gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", clean$Street, perl=TRUE)
+
+
+clean$Street.2 <- gsub("[^A-Za-z ]", " ", clean$Street.2)
+clean$Street.2 <- gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", clean$Street.2, perl=TRUE)
+clean$Street <- gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", clean$Street, perl=TRUE)
+clean$Street.2 <- gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", clean$Street.2, perl=TRUE)
+install.packages('gsubfn')
+install.packages('proto')
+patterns     <- c("Lane", "Road", "Avenue", "Green", "Hospital", "Village", "Center", "Drive", "Circle", "Park","Street")
+replacements <- c("Lan.",  "Rd.", "Ave.", "Gr.","Hosp.","Vil.","Ctr.", "Dr.", "Cr.","Pk.","Str.")
+library('proto)
+library('gsubfn')
+clean$Street <- gsubfn("\\b\\w+\\b", as.list(setNames(replacements, patterns)), clean$Street)
+clean$Street.2 <- gsubfn("\\b\\w+\\b", as.list(setNames(replacements, patterns)), clean$Street.2)
+View(clean)
 
